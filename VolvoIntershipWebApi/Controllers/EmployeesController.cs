51e7cb6fa2ce37773a29ef3256094ccb886f981a
Krzysztof Razik <kynio1997@gmail.com>
2019-05-24 17:28:23 +0200

    Initial WebApi

---
+++ VolvoIntershipWebApi/Controllers/EmployeesController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using VolvoIntershipWebApi.Repositories;
+using VolvoIntershipWebApi.Model;
+using Newtonsoft.Json;
+
+namespace VolvoIntershipWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : Controller
+    {
+        private readonly EmployeeRepository _employeeRepository;
+
+        public EmployeesController(EmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Employee>> Get()
+        {
+            return _employeeRepository.get();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Employee> Get(int id)
+        {
+            return _employeeRepository.get(id);
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult<IEnumerable<Employee>> Delete(int id)
+        {
+            _employeeRepository.delete(id);
+            return _employeeRepository.get();
+        }
+
+        [HttpPost]
+        public ActionResult<IEnumerable<Employee>> Post([FromBody] Employee employee)
+        {
+            _employeeRepository.create(employee);
+            return _employeeRepository.get();
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult<IEnumerable<Employee>> Put([FromBody] Employee employee, int id)
+        {
+            _employeeRepository.modify(id, employee);
+            return _employeeRepository.get();
+        }
+
+    }
+}
---
+++ VolvoIntershipWebApi/Repositories/EmployeeRepository.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VolvoIntershipWebApi.Model;
+
+namespace VolvoIntershipWebApi.Repositories
+{
+    public class EmployeeRepository
+    {
+        public List<Employee> employees;
+
+        public EmployeeRepository()
+        {
+            employees = new List<Employee>()
+            {
+                new Employee()
+                {
+                    Id = 0,
+                    FirstName = "John",
+                    LastName = "Johnson",
+                    Salary = 1500
+                },
+                new Employee()
+                {
+                    Id = 1,
+                    FirstName = "Ann",
+                    LastName = "Mason",
+                    Salary = 1700
+                }
+            };
+        }
+        public void create(Employee employee)
+        {
+            if (employees.Where(e => e.Id == employee.Id).FirstOrDefault() == null)
+            {
+                employees.Add(employee);
+            }
+        }
+
+        public List<Employee> get()
+        {
+            return employees;
+        }
+
+        public Employee get(int id)
+        {
+            return employees.Where(e => e.Id == id).FirstOrDefault();
+        }
+
+        public void modify(int id, Employee employee)
+        {
+            if(employee.FirstName != null &&
+               employee.FirstName != String.Empty &&
+               employee.LastName != null  &&
+               employee.LastName != String.Empty &&
+               employees.Where(e => e.Id == id).FirstOrDefault() != null)
+            {
+                employees.Where(e => e.Id == id).FirstOrDefault().FirstName = employee.FirstName;
+                employees.Where(e => e.Id == id).FirstOrDefault().LastName = employee.LastName;
+                employees.Where(e => e.Id == id).FirstOrDefault().Salary = employee.Salary;
+            }
+        }
+
+        public void delete(int id)
+        {
+            if(employees.Where(e => e.Id == id).FirstOrDefault() != null)
+            {
+                employees.Remove(employees.Where(p => p.Id == id).FirstOrDefault());
+            }
+        }
+    }
+}
